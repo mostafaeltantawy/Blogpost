2fc34ff5c06afd698387db5626eab5b5b4636a6b
ZALTA <54724682+mostafaeltantawy@users.noreply.github.com>
2023-03-03 01:19:15 +0200

    travis commit 8788hounj

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,30 +1,30 @@
 const puppeteer = require("puppeteer");
 
 const Page = require("./helpers/page");
-// let browser, page;
-// beforeEach(async () => {
-//   page = await Page.build();
+let browser, page;
+beforeEach(async () => {
+  page = await Page.build();
 
-//   await page.goto("http://localhost:3000");
-// });
-// afterEach(async () => {
-//   await page.close();
-// });
-
-test("the header has the correct text ", async () => {
-  // const text = await page.getContentsOf("a.brand-logo");
-  expect("lol").toEqual("lol");
+  await page.goto("http://localhost:3000");
+});
+afterEach(async () => {
+  await page.close();
 });
 
-// test("clicking login starts Oauth flow ", async () => {
-//   await page.click(".right a");
-//   const url = await page.url();
-//   expect(url).toMatch(/accounts\.google\.com/);
-// });
+test("the header has the correct text ", async () => {
+  const text = await page.getContentsOf("a.brand-logo");
+  expect(text).toEqual("Blogster");
+});
 
-// test("Wheen signed in , shows logout button", async () => {
-//   await page.login();
+test("clicking login starts Oauth flow ", async () => {
+  await page.click(".right a");
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
 
-//   const text = await page.getContentsOf("a[href='/auth/logout']");
-//   expect(text).toEqual("Logout");
-// });
+test("When signed in , shows logout button", async () => {
+  await page.login();
+
+  const text = await page.getContentsOf("a[href='/auth/logout']");
+  expect(text).toEqual("Logout");
+});
