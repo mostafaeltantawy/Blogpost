925a190e7a1419b844888ca38c78674b9471eef6
ZALTA <54724682+mostafaeltantawy@users.noreply.github.com>
2023-03-03 16:32:19 +0200

    travis commit SS

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,5 @@
-jest.setTimeout(300000);
+jest.setTimeout(30000);
+console.log(process.env.NODE_ENV);
 require("../models/User");
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
