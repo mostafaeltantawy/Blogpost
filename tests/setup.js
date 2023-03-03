07539b694d161c4bea1d65e68992878461d8e956
ZALTA <54724682+mostafaeltantawy@users.noreply.github.com>
2023-03-03 16:44:09 +0200

    travis commit SSSs

--- tests/setup.js
+++ tests/setup.js
@@ -3,5 +3,6 @@ console.log(process.env.NODE_ENV);
 require("../models/User");
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
+console.log(keys.mongoURI);
 
 mongoose.connect(keys.mongoURI);
